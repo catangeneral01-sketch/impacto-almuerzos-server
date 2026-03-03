1bdaa9fe98a8b9948410828c68ea0ac3d30cfb84
catangeneral01-sketch <catan.general01@gmail.com>
2026-03-03 11:35:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -104,4 +104,3 @@ app.get("/", (req, res) => res.status(200).send("Impacto Almuerzos Server OK"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on", PORT));
-```
